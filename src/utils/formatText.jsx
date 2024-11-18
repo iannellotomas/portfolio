8228df02ae35dc24da01a4283b3b7157b9d82abd
Tomás Iannello <106415807+iannellotomas@users.noreply.github.com>
2024-11-18 16:40:30 -0300

    Delete src directory

--- src/utils/formatText.jsx
+++
@@ -1,5 +0,0 @@
-export function formatText(text) {
-  // Expresión regular para encontrar "**texto**" y reemplazarlo por "<strong>texto</strong>"
-  const regex = /\*\*(.*?)\*\*/g;
-  return text.replace(regex, "<strong>$1</strong>");
-}
